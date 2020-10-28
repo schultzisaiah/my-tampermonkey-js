4e3e3fac44b196e244b46a2c8ff5a76a1d334e10
Isaiah <52469612+IsaiahSchultzTR@users.noreply.github.com>
2020-10-28 11:18:49 -0500

    adding error-handling

--- ld-dev-tools.js
+++ ld-dev-tools.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         LD Dev Tools
-// @version      0.10
+// @version      0.11
 // @description  try to take over the world!
 // @author       Isaiah Schultz
 // @run-at       document-idle
@@ -14,65 +14,73 @@
 
 (function() {
     'use strict';
+    try {
+        // Build Number:///////////////////////////////////////////////////////////////////////////////////////
+        var buildNumTxt = 'Build: unknown';
+        if (typeof BuildNumber !== 'undefined') {
+            buildNumTxt = BuildNumber;
+        }
+        var buildNumDiv = document.createElement('pre');
+        buildNumDiv.style.cssText = 'position: fixed;left: 5%;top: 1%;padding: 3px;color: black;font-size: x-small;background-color: white;opacity: 75%;';
+        buildNumDiv.appendChild(document.createTextNode(buildNumTxt));
+        document.body.appendChild(buildNumDiv);
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    // Build Number:///////////////////////////////////////////////////////////////////////////////////////
-    var buildNumTxt = 'Build: unknown';
-    if (typeof BuildNumber !== 'undefined') {
-        buildNumTxt = BuildNumber;
-    }
-    var buildNumDiv = document.createElement('pre');
-    buildNumDiv.style.cssText = 'position: fixed;left: 5%;top: 1%;padding: 3px;color: black;font-size: x-small;background-color: white;opacity: 75%;';
-    buildNumDiv.appendChild(document.createTextNode(buildNumTxt));
-    document.body.appendChild(buildNumDiv);
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////
+        // DATA LAYER DISPLAY://///////////////////////////////////////////////////////////////////////////////
+        var dataLayerText = 'FLDataLayer = ' + JSON.stringify(FLDataLayer,null,1)
+        if (typeof FlagsFLFE !== 'undefined') {
+            dataLayerText += ';\nFlagsFLFE = ' + JSON.stringify(FlagsFLFE,null,1) + ';';
+        }
+        var dataLayerDiv = document.createElement('pre');
+        dataLayerDiv.style.cssText = 'position: fixed;left: 86%;top: .6%;padding: 3px;color: black;background-color: lightgreen;width: 10%;font-size: x-small;opacity: 75%;';
+        dataLayerDiv.appendChild(document.createTextNode(dataLayerText));
+        document.body.appendChild(dataLayerDiv);
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    // DATA LAYER DISPLAY://///////////////////////////////////////////////////////////////////////////////
-    var dataLayerText = 'FLDataLayer = ' + JSON.stringify(FLDataLayer,null,1)
-    if (typeof FlagsFLFE !== 'undefined') {
-        dataLayerText += ';\nFlagsFLFE = ' + JSON.stringify(FlagsFLFE,null,1) + ';';
-    }
-    var dataLayerDiv = document.createElement('pre');
-    dataLayerDiv.style.cssText = 'position: fixed;left: 86%;top: .6%;padding: 3px;color: black;background-color: lightgreen;width: 10%;font-size: x-small;opacity: 75%;';
-    dataLayerDiv.appendChild(document.createTextNode(dataLayerText));
-    document.body.appendChild(dataLayerDiv);
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////
+        // SRP CACHE WOKER? ///////////////////////////////////////////////////////////////////////////////////
+        var srpDiv = document.getElementById('serp_results');
+        //if (srpDiv !== 'undefined' && srpDiv !== null && srpDiv.innerHTML !== null && String(srpDiv.innerHTML).includes('small-order-')) {
+        if (srpDiv !== 'undefined' && srpDiv.innerHTML !== null && String(srpDiv.innerHTML).includes('small-order-')) {
+            var cfCacheWorkerTag = document.createElement('div');
+            cfCacheWorkerTag.style.cssText = 'position: fixed;left: 5%;top: 6%;padding: 3px;color: black;font-size: x-small;background-color: orange;opacity: 75%;';
+            cfCacheWorkerTag.appendChild(document.createTextNode('CF SRP cache worker ran!'));
+            document.body.appendChild(cfCacheWorkerTag);
+        }
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    // SRP CACHE WOKER? ///////////////////////////////////////////////////////////////////////////////////
-    var srpDiv = document.getElementById('serp_results');
-    if (srpDiv !== 'undefined' && srpDiv !== null && srpDiv.innerHTML !== null && String(srpDiv.innerHTML).includes('small-order-')) {
-        var cfCacheWorkerTag = document.createElement('div');
-        cfCacheWorkerTag.style.cssText = 'position: fixed;left: 5%;top: 6%;padding: 3px;color: black;font-size: x-small;background-color: orange;opacity: 75%;';
-        cfCacheWorkerTag.appendChild(document.createTextNode('CF SRP cache worker ran!'));
-        document.body.appendChild(cfCacheWorkerTag);
-    }
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////
+        // LD or PVIEW?////////////////////////////////////////////////////////////////////////////////////////
+        if (String(document.location).includes('/profile/')) {
+            GM_xmlhttpRequest({
+                method: "GET",
+                url: document.location,
+                onload: function(response) {
+                    var newDiv = document.createElement('div');
+                    var style = 'background-color: red;';
+                    var textVal = 'error';
 
-    // LD or PVIEW?////////////////////////////////////////////////////////////////////////////////////////
-    if (String(document.location).includes('/profile/')) {
-        GM_xmlhttpRequest({
-            method: "GET",
-            url: document.location,
-            onload: function(response) {
-                var newDiv = document.createElement('div');
-                var style = 'background-color: red;';
-                var textVal = 'error';
+                    if (response.responseHeaders.includes('ldprofile: true')) {
+                        textVal = '- LD -';
+                        style = "background-color: lightgreen;"
+                    } else {
+                        textVal = '- PVIEW -';
+                        style = "background-color: lightblue;"
+                    }
 
-                if (response.responseHeaders.includes('ldprofile: true')) {
-                    textVal = '- LD -';
-                    style = "background-color: lightgreen;"
-                } else {
-                    textVal = '- PVIEW -';
-                    style = "background-color: lightblue;"
+                    newDiv.style.cssText = 'position: fixed;left: 47%;top: 1%;padding: 3px;color: black;font-weight: bolder;' + style;
+
+                    var newContent = document.createTextNode(textVal);
+                    newDiv.appendChild(newContent);
+
+                    document.body.appendChild(newDiv);
                 }
-
-                newDiv.style.cssText = 'position: fixed;left: 47%;top: 1%;padding: 3px;color: black;font-weight: bolder;' + style;
-
-                var newContent = document.createTextNode(textVal);
-                newDiv.appendChild(newContent);
-
-                document.body.appendChild(newDiv);
-            }
-        });
+            });
+        }
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////
     }
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////
+    catch(err) {
+        var errorDiv = document.createElement('pre');
+        errorDiv.style.cssText = 'position: fixed;left: 1%;bottom: 1%;padding: 3px;color: black;background-color: indianred;width: 98%;font-size: x-small;opacity: 75%;font-weight: bolder;';
+        errorDiv.appendChild(document.createTextNode('LD DEV TOOLS ERROR:\n' + err.message));
+        document.body.appendChild(errorDiv);
+    }
 })();
