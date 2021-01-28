66cd86ffc4b73cf7bf8a0a504544fe1c7e4b67f0
Isaiah <52469612+IsaiahSchultzTR@users.noreply.github.com>
2021-01-27 20:18:55 -0600

    adding FL data object as well

--- ld-dev-tools.js
+++ ld-dev-tools.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         LD Dev Tools
-// @version      0.11
+// @version      0.12
 // @description  try to take over the world!
 // @author       Isaiah Schultz
 // @run-at       document-idle
@@ -27,12 +27,15 @@
         ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
         // DATA LAYER DISPLAY://///////////////////////////////////////////////////////////////////////////////
-        var dataLayerText = 'FLDataLayer = ' + JSON.stringify(FLDataLayer,null,1)
+        var dataLayerText = 'FLDataLayer = ' + JSON.stringify(FLDataLayer,null,1) + ';'
+        if (typeof FL !== 'undefined') {
+            dataLayerText += '\nFL = ' + JSON.stringify(FL,null,1) + ';';
+        }
         if (typeof FlagsFLFE !== 'undefined') {
-            dataLayerText += ';\nFlagsFLFE = ' + JSON.stringify(FlagsFLFE,null,1) + ';';
+            dataLayerText += '\nFlagsFLFE = ' + JSON.stringify(FlagsFLFE,null,1) + ';';
         }
         var dataLayerDiv = document.createElement('pre');
-        dataLayerDiv.style.cssText = 'position: fixed;left: 86%;top: .6%;padding: 3px;color: black;background-color: lightgreen;width: 10%;font-size: x-small;opacity: 75%;';
+        dataLayerDiv.style.cssText = 'position: fixed;left: 86%;top: .6%;padding: 3px;color: black;background-color: lightgreen;width: 10%;font-size: x-small;opacity: 75%; max-height: 97%;';
         dataLayerDiv.appendChild(document.createTextNode(dataLayerText));
         document.body.appendChild(dataLayerDiv);
         ///////////////////////////////////////////////////////////////////////////////////////////////////////
