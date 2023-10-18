dfc37c09cc24e2549929b869fe121e8d16962014
Isaiah <52469612+IsaiahSchultzTR@users.noreply.github.com>
2023-10-18 16:15:19 -0500

    Update ld-dev-tools.js
    adding distinction for "localhost"

--- ld-dev-tools.js
+++ ld-dev-tools.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         LD Dev Tools
-// @version      0.31
+// @version      0.32
 // @description  try to take over the world!
 // @author       Isaiah Schultz
 // @run-at       document-idle
@@ -58,6 +58,15 @@
         ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
         // DATA LAYER DISPLAY://///////////////////////////////////////////////////////////////////////////////
+        var color = "lightgreen";
+        try {
+            var hostVal = FL.local.hosts.lawyers
+            if (hostVal.includes("localhost")) {
+                color = "yellow";
+            }
+        } catch (err) {
+            // oh well
+        }
         var dataLayerText = 'FLDataLayer = ' + JSON.stringify(FLDataLayer,null,1) + ';'
         if (typeof FL !== 'undefined') {
             dataLayerText += '\nFL = ' + JSON.stringify(FL,null,1) + ';';
@@ -80,7 +89,7 @@
             dataLayerText += '\nFlagsFLFE = ' + JSON.stringify(FlagsFLFE,null,1) + ';';
         }
         var dataLayerDiv = document.createElement('pre');
-        dataLayerDiv.style.cssText = 'position: fixed;left: 86%;top: .6%;padding: 3px;color: black;background-color: lightgreen;width: 10%;font-size: x-small;opacity: 75%; max-height: 97%; z-index: 9999';
+        dataLayerDiv.style.cssText = 'position: fixed;left: 86%;top: .6%;padding: 3px;color: black;background-color: ' + color + ';width: 10%;font-size: x-small;opacity: 75%; max-height: 97%; z-index: 9999';
         dataLayerDiv.appendChild(document.createTextNode(dataLayerText));
         document.body.appendChild(dataLayerDiv);
         ///////////////////////////////////////////////////////////////////////////////////////////////////////
